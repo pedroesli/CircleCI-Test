0d4a546a1e6aaef2898f31fb1604eed6f4b65f83
Pedro Esli <pedroesli.vn@gmail.com>
2022-07-14 12:20:25 -0300

    Project commit

---
+++ Circle-Ci-Test-App/Circle-Ci-Test-App/Circle_Ci_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circle_Ci_Test_AppApp.swift
+//  Circle-Ci-Test-App
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Circle_Ci_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circle-Ci-Test-App/Circle-Ci-Test-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Circle-Ci-Test-App
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 14/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
